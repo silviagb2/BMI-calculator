bbe7364083e2822da6636cae58647f1446ecf558
silviagb2 <silviagb2@gmail.com>
2021-01-11 14:07:57 +0100

    finished

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-$(function() {
-    $("form" ).submit(function( event ) {
+$(function(){
+    $("form").submit(function( event ) {
 
         event.preventDefault();
-        
-        let height = $('#height').val()
+
+        let height = $("#height").val()
         let weight = $('#weight').val()
 
-        let imc = (weight / (height * height) * 10000).toFixed(2)
+        const imc = (weight / ((height*height)/10000))
+        console.log(imc)
 
-      $('#results').text(imc)
-        
-      if (imc<18.6) {
-        $('p:nth-of-type(1)').css('color', 'red')
-      }
+        let results =  $("#results")
+        results.text(imc.toFixed(2))
 
-      else if (imc>=18.6 && imc<=24.9) {
-        $('p:nth-of-type(2)').css('color', 'green')
-      }
-
-      else {
-        $('p:nth-of-type(3)').css('color', 'red')
-      }
-      
-    });
-}) 
+        if(imc<18.6 || imc>24.9){
+            results.css("color","red");
+        }else{
+            results.css("color","green");
+        }
+    })
+  })
